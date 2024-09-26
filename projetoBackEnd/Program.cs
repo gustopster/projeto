b3c548b52ded0f4f45f45049b582b6f9fa5282e5
gustopster <gustavogomes763@gmail.com>
2024-09-26 19:40:26 -0300

    Tentativa de subir para o azure

--- projetoBackEnd/Program.cs
+++ projetoBackEnd/Program.cs
@@ -13,16 +13,10 @@ builder.Services.AddScoped<AnimalService>();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
