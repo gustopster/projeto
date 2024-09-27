aa80be7e31e996cccd5e19f0eeca5decb3e7470d
gustopster <gustavogomes763@gmail.com>
2024-09-27 01:42:43 -0300

    definindo as rotas corretas

--- projetoFrontEnd/src/services/api.ts
+++ projetoFrontEnd/src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: 'https://backendprojeto-dtehecguhwdufucw.brazilsouth-01.azurewebsites.net',
+    /* baseURL: 'http://localhost:5284' */
     timeout: 10000,
 });
 
