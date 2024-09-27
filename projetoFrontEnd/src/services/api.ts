00bcaa88c97d475407801e7cb365abcf95c4f66e
gustopster <gustavogomes763@gmail.com>
2024-09-27 01:50:16 -0300

    Definindo a rota dinamicamente

--- projetoFrontEnd/src/services/api.ts
+++ projetoFrontEnd/src/services/api.ts
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://backendprojeto-dtehecguhwdufucw.brazilsouth-01.azurewebsites.net',
-    /* baseURL: 'http://localhost:5284' */
+    baseURL: window.location.href === 'http://localhost:5173/projeto/' ? 'http://localhost:5284' : 'https://backendprojeto-dtehecguhwdufucw.brazilsouth-01.azurewebsites.net',
     timeout: 10000,
 });
-
+console.log(window.location.href)
 export default api;
