9a5ea5d09278fb0543c9d2c23e4f8c10f578fd0b
gustopster <gustavogomes763@gmail.com>
2024-10-03 23:25:40 -0300

    Correção na pasta raiz

--- projetoFrontEnd/src/services/api.ts
+++ projetoFrontEnd/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: window.location.href === 'http://localhost:5173/animais/'
+  baseURL: window.location.href === 'http://localhost:5173/projeto/'
     ? 'http://localhost:5284'
     : 'https://backendprojeto-dtehecguhwdufucw.brazilsouth-01.azurewebsites.net',
   timeout: 10000,
